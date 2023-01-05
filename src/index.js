556af422ea2e035a8a2fc627382a6bdf5b40f406
Zacherman, Avi M <avi.zacherman@ecoatm.com>
2023-01-05 11:05:01 -0500

    Fix typo

--- src/index.js
+++ src/index.js
@@ -14,10 +14,9 @@ async function startApolloServer(typeDefs, resolvers) {
     },
   });
 
-  await server.listen({ port: process.env.PORT || 4000 });
+  const { url } = await server.listen({ port: process.env.PORT || 4000 });
   console.log(`
       🚀  Server is running
-      🔉  Listening on port ${port}
       📭  Query at ${url}
     `);
 }
